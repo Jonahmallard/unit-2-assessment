906496bc2d2db496a14b605835fa6b7b84218db0
Jonah Mallard <benjaminjmallard@gmail.com>
2020-12-09 14:36:39 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Unit 2 Assessment' });
+});
+
+module.exports = router;
